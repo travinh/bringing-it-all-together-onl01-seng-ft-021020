a1d15a23527c3b1c16bdf15d2ffa864d96d013a8
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 23:34:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -84,7 +84,20 @@ class Dog
   end
   
   def find_or_create_by(name:, breed:)
-    sql = <
+    sql = <<-SQL
+    SELECT * FROM dogs
+    WHERE name = ? AND breed = ?
+    SQL
+
+
+    dog = DB[:conn].execute(sql, name, breed).first
+
+    if dog
+      new_dog = self.new_from_db(dog)
+    else
+      new_dog = self.create({:name => name, :breed => breed})
+    end
+    new_dog
   end
   
 end
